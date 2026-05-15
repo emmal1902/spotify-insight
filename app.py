3cfa1511748b309d18628a5c97eccd7dec3802dd
emmal1902 <emmaaleclerc@yahoo.com>
2026-05-15 14:28:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from src.auth import get_spotify_client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print("CLIENT_ID =", os.getenv("SPOTIPY_CLIENT_ID"))
 
 def main():
     sp = get_spotify_client()
