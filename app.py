f2b1187070b397ef350a31763c4f24c9f5b4795b
emmal1902 <emmaaleclerc@yahoo.com>
2026-05-15 13:58:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from src.auth import get_spotify_client
-from src.data_fetch import get_top_tracks
-from src.analysis import tracks_to_df
 
 def main():
     sp = get_spotify_client()
 
-    tracks = get_top_tracks(sp)
-    df = tracks_to_df(tracks)
-
-    print(df.head())
+    user = sp.current_user()
+    print("🎧 Connecté en tant que :", user["display_name"])
+    print("ID :", user["id"])
 
 if __name__ == "__main__":
     main()
