8f20988086c610d7811c53107cf3b0ed08f64e05
emmal1902 <emmaaleclerc@yahoo.com>
2026-05-15 13:49:43 -0400

    Create auth.py

---
+++ src/auth.py
@@ -0,0 +1,14 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_spotify_client():
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+        scope="user-top-read user-read-recently-played"
+    ))
